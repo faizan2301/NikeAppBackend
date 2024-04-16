cbe62ce5b583d81dc62555f9f27ebfc6fb2abbc1
faizan2301 <skfaizan2301@gmail.com>
2024-04-16 17:21:44 +0530

    🦊ok

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -4,15 +4,25 @@ const stripe = require("stripe")(process.env.SECRETKEY);
 
 router.post("/intent", async (req, res) => {
   try {
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: "2024-04-10" }
+    );
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount,
       currency: "usd",
+      customer: customer.id,
       automatic_payment_methods: {
         enabled: true,
       },
     });
 
-    res.json({ paymentIntent: paymentIntent.client_secret });
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      customer: customer.id,
+      ephemeralKey: ephemeralKey.secret,
+    });
   } catch (e) {
     res.status(400).json({
       status: "FAILED",
